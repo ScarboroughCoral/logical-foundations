16cfe7e6c4380e5d789fcf35a28da0ce2a800e26
Mingyue Li <3249977074@qq.com>
2023-07-16 20:34:21 +0800

    feat(list): finish standard (snd_fst_is_swap),optional (fst_swap_is_snd)

---
+++ List.v
@@ -0,0 +1,106 @@
+From LF Require Export Induction.
+
+Module NatList.
+
+Inductive natprod: Type :=
+  | pair (n1 n2 : nat).
+
+Check (pair 3 5) : natprod.
+
+Definition fst (p : natprod) : nat :=
+  match p with
+  | pair x y => x
+  end.
+Definition snd (p : natprod) : nat :=
+  match p with
+  | pair x y => y
+  end.
+Compute (fst (pair 3 5)).
+
+Notation "( x , y )" := (pair x y).
+
+Compute (fst (3,5)).
+
+Definition fst' (p : natprod) : nat :=
+  match p with
+  | (x,y) => x
+  end.
+Definition snd' (p : natprod) : nat :=
+  match p with
+  | (x,y) => y
+  end.
+Definition swap_pair (p : natprod) : natprod :=
+  match p with
+  | (x,y) => (y,x)
+  end.
+
+Theorem surjective_pairing' : forall (n m : nat),
+  (n,m) = (fst (n,m), snd (n,m)).
+Proof.
+  reflexivity. Qed.
+
+Theorem surjective_pairing_stuck : forall (p : natprod),
+  p = (fst p, snd p).
+Proof.
+  simpl. (* Doesn't reduce anything! *)
+Abort.
+
+(* 
+Instead, we need to expose the structure of p so that simpl can perform the pattern match in fst and snd. We can do this with destruct.
+Notice that, unlike its behavior with nats, where it generates two subgoals, destruct generates just one subgoal here. That's because natprods can only be constructed in one way.
+ *)
+Theorem surjective_pairing : forall (p : natprod),
+  p = (fst p, snd p).
+Proof.
+  intros p. destruct p as [n m]. simpl. reflexivity. Qed.
+
+
+(* Exercise: 1 star, standard (snd_fst_is_swap) *)
+Theorem snd_fst_is_swap : forall (p : natprod),
+  (snd p, fst p) = swap_pair p.
+Proof.
+  intros p. destruct p. simpl. reflexivity.
+Qed.
+(* End Exercise: 1 star, standard (snd_fst_is_swap) *)
+
+(* Exercise: 1 star, standard, optional (fst_swap_is_snd) *)
+Theorem fst_swap_is_snd : forall (p : natprod),
+  fst (swap_pair p) = snd p.
+Proof.
+  intros p. destruct p. simpl. reflexivity.
+Qed.
+(* End Exercise: 1 star, standard, optional (fst_swap_is_snd) *)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
