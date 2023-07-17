c60fbcc21ad3ae2518b103ecd822c46cda6f9fb1
Mingyue Li <3249977074@qq.com>
2023-07-17 21:37:56 +0800

    feat(list): finish especially useful (list_funs)

--- List.v
+++ List.v
@@ -71,7 +71,114 @@ Proof.
 Qed.
 (* End Exercise: 1 star, standard, optional (fst_swap_is_snd) *)
 
+Inductive natlist : Type :=
+  | nil
+  | cons (n : nat) (l : natlist).
 
+Definition mylist := cons 1 (cons 2 (cons 3 nil)).
+
+Check mylist.
+
+Compute mylist.
+
+Notation "x :: l" := (cons x l)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
+Definition mylist1 := 1 :: (2 :: (3 :: nil)).
+Definition mylist2 := 1 :: 2 :: 3 :: nil.
+Definition mylist3 := [1;2;3].
+
+Compute mylist1.
+Compute mylist2.
+Compute mylist3.
+
+Fixpoint repeat (n count : nat) : natlist :=
+  match count with
+  | O => nil
+  | S count' => n :: (repeat n count')
+  end.
+
+Fixpoint length (l:natlist) : nat :=
+  match l with
+  | nil => O
+  | h :: t => S (length t)
+  end.
+
+Fixpoint app (l1 l2 : natlist) : natlist :=
+  match l1 with
+  | nil => l2
+  | h :: t => h :: (app t l2)
+  end.
+
+Notation "x ++ y" := (app x y)
+                     (right associativity, at level 60).
+Example test_app1: [1;2;3] ++ [4;5] = [1;2;3;4;5].
+Proof. reflexivity. Qed.
+Example test_app2: nil ++ [4;5] = [4;5].
+Proof. reflexivity. Qed.
+Example test_app3: [1;2;3] ++ nil = [1;2;3].
+Proof. reflexivity. Qed.
+
+Definition hd (default : nat) (l : natlist) : nat :=
+  match l with
+  | nil => default
+  | h :: t => h
+  end.
+
+Definition tl (l : natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => t
+  end.
+
+Example test_hd1: hd 0 [1;2;3] = 1.
+Proof. reflexivity. Qed.
+Example test_hd2: hd 0 [] = 0.
+Proof. reflexivity. Qed.
+Example test_tl: tl [1;2;3] = [2;3].
+Proof. reflexivity. Qed.
+
+(* Exercise: 2 stars, standard, especially useful (list_funs) *)
+Fixpoint nonzeros (l:natlist) : natlist :=
+match l with
+  | nil => nil
+  | 0::t => nonzeros t
+  | x::l' => x :: nonzeros l'
+end.
+Example test_nonzeros:
+  nonzeros [0;1;0;2;3;0;0] = [1;2;3].
+Proof. simpl. reflexivity. Qed.
+Fixpoint oddmembers (l:natlist) : natlist :=
+match l with
+  | nil => nil
+  | x::l' => match odd x with
+              | true => x::oddmembers l'
+              | false => oddmembers l'
+             end
+end.
+Example test_oddmembers:
+  oddmembers [0;1;0;2;3;0;0] = [1;3].
+Proof. simpl. reflexivity. Qed.
+
+Definition countoddmembers (l:natlist) : nat := length (oddmembers l).
+Example test_countoddmembers1:
+  countoddmembers [1;0;3;1;4;5] = 4.
+Proof. 
+  reflexivity.
+Qed.
+Example test_countoddmembers2:
+  countoddmembers [0;2;4] = 0.
+Proof.
+  reflexivity.
+Qed.
+Example test_countoddmembers3:
+  countoddmembers nil = 0.
+Proof. 
+  reflexivity.
+Qed.
+(* End Exercise: 2 stars, standard, especially useful (list_funs) *)
 
 
 
