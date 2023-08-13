74a3720f1b3b82f1dbb75f71f65c5e53b77dc3e4
Mingyue Li <3249977074@qq.com>
2023-08-13 17:40:43 +0800

    feat(poly): standard (poly_exercises)

--- Poly.v
+++ Poly.v
@@ -65,14 +65,106 @@ c = No
 End MumbleGrumble.
 (* End Exercise: 2 stars, standard, optional (mumble_grumble) *)
 
+Fixpoint repeat' X x count : list X :=
+  match count with
+  | 0 => nil X
+  | S count' => cons X x (repeat' X x count')
+  end.
 
+Check repeat' : forall X : Type, X -> nat -> list X.
 
+Check repeat'.
 
+Fixpoint repeat'' X x count : list X :=
+match count with
+| 0 => nil _
+| S count' => cons _ x (repeat'' _ x count')
+end.
 
+Arguments nil {X}.
+Arguments cons {X}.
+Arguments repeat {X}.
 
+Definition list123'' := cons 1 (cons 2 (cons 3 nil)).
 
+Fixpoint repeat''' {X : Type} (x : X) (count : nat) : list X :=
+  match count with
+  | 0 => nil
+  | S count' => cons x (repeat''' x count')
+  end.
 
+Fixpoint app {X : Type} (l1 l2 : list X) : list X :=
+match l1 with
+| nil => l2
+| cons h t => cons h (app t l2)
+end.
 
+Fixpoint rev {X:Type} (l:list X) : list X :=
+  match l with
+  | nil => nil
+  | cons h t => app (rev t) (cons h nil)
+  end.
+Fixpoint length {X : Type} (l : list X) : nat :=
+  match l with
+  | nil => 0
+  | cons _ l' => S (length l')
+  end.
+Example test_rev1 :
+  rev (cons 1 (cons 2 nil)) = (cons 2 (cons 1 nil)).
+Proof. reflexivity. Qed.
+
+Example test_rev2:
+  rev (cons true nil) = cons true nil.
+Proof. reflexivity. Qed.
+
+Example test_length1: length (cons 1 (cons 2 (cons 3 nil))) = 3.
+Proof. reflexivity. Qed.
+
+Fail Definition mynil := nil.
+
+Definition mynil : list nat := nil.
+
+Check nil.
+
+Check @nil.
+
+Definition mynil' := @nil nat.
+
+Notation "x :: y" := (cons x y)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y []) ..).
+Notation "x ++ y" := (app x y)
+                     (at level 60, right associativity).
+
+Definition list123''' := [1; 2; 3].
+
+(* Exercise: 2 stars, standard (poly_exercises) *)
+Theorem app_nil_r : forall (X:Type), forall l:list X,
+  l ++ [] = l.
+Proof.
+  intros X l.
+  induction l.
+  - reflexivity.
+  - simpl. rewrite IHl. reflexivity.
+Qed.
+Theorem app_assoc : forall A (l m n:list A),
+  l ++ m ++ n = (l ++ m) ++ n.
+Proof.
+  intros A l m n.
+  induction l.
+  - simpl. reflexivity.
+  - simpl. rewrite IHl. reflexivity.
+Qed.
+Lemma app_length : forall (X:Type) (l1 l2 : list X),
+  length (l1 ++ l2) = length l1 + length l2.
+Proof.
+  intros X l1 l2.
+  induction l1.
+  - simpl. reflexivity.
+  - simpl. rewrite IHl1. reflexivity.
+Qed.
+(* End Exercise: 2 stars, standard (poly_exercises) *)
 
 
 
